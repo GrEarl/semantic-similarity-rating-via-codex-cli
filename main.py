2230c00aca5f91cc9bad7b0a35c24ecca55df24c
GrEarl <raibonngb@gmail.com>
2025-11-07 02:07:54 +0000

    for ssr

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import asyncio
 import subprocess
+import numpy as np
+import polars as po
+
 
 app = FastAPI()
 
@@ -8,3 +11,9 @@ app = FastAPI()
 async def top_page():
     return {"message": "Anything is in here...Now time"}
 
+@app.post("/tasks")
+async def task_generator():
+    pass
+
+async def task_compiler(task_id, llm_response, SSR_Model):
+    pass
